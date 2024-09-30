1795cd4c4292983d6ea14f8743fe609d4a578440
saadyasser <saad.wadi.229@gmail.com>
2024-09-30 20:05:23 +0300

    create prodcut cart component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import { Product } from "../types";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
+  return (
+    <article>
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <span>{product.category}</span>
+      <p className="product-price">${product.price}</p>
+    </article>
+  );
+};
+
+export default ProductCard;
